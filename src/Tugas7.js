977e8e3711ad5f7bed3d1b5af4779314a235d3db
AhmadSibgatullah <ahmad851133@smk.belajar.id>
2024-05-06 13:27:05 +0700

    Tugas 7 reach JS

---
+++ src/Tugas7.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Tugas7() {
+  return (
+    <div>
+      <div className="container">
+        <div className="box">
+          <h1>Data Diri Kelas ReactJS</h1>
+          <hr></hr>
+          <ul>
+            <li><b>Nama Lengkap :</b> Ahmad Sibgatullah L</li>
+            <li><b>Email :</b> ahmad851133@smk.belajar.id</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tugas7;
